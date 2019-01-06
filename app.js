8d89b817194116232d0e59ad9e56bee8070c7a70
Achsuthan <achsuthan@icloud.com>
2019-01-06 12:15:06 +0530

    fs package added > ach

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const logger = require('./logger');
 const eventManagement = require('./emitter')
 logger.endPoint('This is API call');
 
+/*
+    This is event package
+ */
+
 //handling the event management
 const eventEmitter = require('events');
 //eventEmitter is a class so we have to create a object for it 
@@ -17,9 +21,30 @@ emitter.emit('Event','values')
 
 //emitter.on() function should difine before the emiiter.emit() function
 
-
-
-
 eventManagement.EventEmitterFunc('Test Event', 'Values of paramters')
+
+
+/*
+    This is file package 
+ */
+
+ //always use async task, beacse in the sync will block the process until finish the task
+ const fs = require('fs');
+
+ const files = fs.readdirSync('./')
+ console.log(files);
+
+
+ //by using async task get all the files from a directories 
+fs.readdir('./', function(err,files) {
+    if (err){
+        console.log("Error : "+ err);
+    }
+    else {
+        console.log(files);
+    }
+})
+
+ 
 
 
