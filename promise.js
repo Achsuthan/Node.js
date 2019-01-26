9fdad316e9bd1408094f4f3f6fca1077d735ba7a
Achsuthan <achsuthan@icloud.com>
2019-01-27 00:48:15 +0530

    Parallel Promise added > ach

--- promise.js
+++ promise.js
@@ -15,3 +15,26 @@ const pp = new Promise((resolve,reject) =>{
 })
 
 pp.then(result =>console.log("result")).catch((error)=> console.log(error.message))
+
+
+//Parallel Promise
+
+//this p promise is used to send the result
+const p1 = new Promise((resolve,reject) =>{
+    setTimeout((handler)=>{
+        console.log("Test P1")
+        resolve(1)
+    },2000)
+})
+
+
+//this is used for fail
+const p2 = new Promise((resolve,reject) =>{
+    setTimeout((handler)=>{
+        reject(new Error("Hello error"))
+    },2000)
+})
+
+Promise.all([p1,p2])
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message))
